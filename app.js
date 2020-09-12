07b7ab00df4a2d6b8f827c10e00cf6c8231397c4
Wojciech Odachowski <wojtek199718@gmail.com>
2020-09-12 15:05:55 +0200

    initial db config

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  return 'Hello World!';
+  res.send('Hello World!');
 })
 
 // catch 404 and forward to error handler
